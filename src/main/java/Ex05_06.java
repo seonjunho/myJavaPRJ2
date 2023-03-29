d7e778160516c6325110e19ae9963ed32461cfa2
seonjunho <windworm119@gmail.com>
2023-03-29 10:24:54 +0900

    EX05_06 학점계산기

--- src/main/java/Ex05_06.java
+++ src/main/java/Ex05_06.java
@@ -1,17 +1,36 @@
 import java.util.Scanner;
+
 public class Ex05_06 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a;
 
-        System.out.printf("정수를 입력하세요 : ");
+        System.out.printf("점수를 입력하세요 : ");
         a = s.nextInt();
 
-        if(a % 2 == 0) {
-            System.out.printf("짝수를 입력했군요..\n");
-        }else{
-            System.out.printf("홀수를 입력했군요..\n");
+        if (a >= 90) {
+            System.out.printf("A");
+        } else {
+            if (a >= 80) {
+                System.out.printf("B");
+            } else {
+                if (a >= 70) {
+                    System.out.printf("C");
+                } else {
+                    if (a >= 60) {
+                        System.out.printf("D");
+                    } else {
+                        System.out.printf("F");
+                    }
+                }
+            }
+            System.out.printf("학점 입니다. \n");
         }
-
     }
 }
+
+
+
+
+
+
