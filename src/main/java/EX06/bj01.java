cf01ca9e999fd26324290edf8eac49d618607579
seonjunho <windworm119@gmail.com>
2023-04-05 08:51:51 +0900

    2023.04.05

---
+++ src/main/java/EX06/bj01.java
@@ -0,0 +1,20 @@
+// 1과 3을 입력하여 0.3333333333333333으로 출력하기
+package EX06;
+
+import java.util.Scanner;
+
+public class bj01 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        double a = s.nextDouble();
+
+        double b = s.nextDouble();
+
+        System.out.println(a / b);
+
+
+    }
+
+}
