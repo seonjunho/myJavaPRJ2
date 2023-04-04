44cc9e6d6fdf50f677eb8da7a925d2fbe40258aa
seonjunho <windworm119@gmail.com>
2023-04-04 11:12:38 +0900

    2023.04.04

---
+++ src/main/java/EX06/New_bc01.java
@@ -0,0 +1,13 @@
+// 77 + 77 + 7777 = 7931 출력하기
+package EX06;
+
+import java.util.Scanner;
+
+public class New_bc01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+
+    }
+}
